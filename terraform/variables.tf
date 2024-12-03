f362d92c070feff3d020740776256f22a237e68c
YuanFangXiBei <shilei12369@163.com>
2024-12-03 16:08:23 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,10 +17,12 @@ variable "publisher_email" {
 variable "publisher_name" {
   description = "Name of the API Management publisher"
 }
+
 variable "sku_name" {
   description = "SKU name for the API Management service"
   default     = "Developer_1"
 }
+
 variable "client_id" {
   description = "Azure Service Principal client ID"
 }
@@ -36,4 +38,3 @@ variable "subscription_id" {
 variable "tenant_id" {
   description = "Azure tenant ID"
 }
-

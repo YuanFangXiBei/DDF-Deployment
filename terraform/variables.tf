99d716804ebdc5ff0b037ff8bc8a852d2e3a4239
YuanFangXiBei <shilei12369@163.com>
2024-12-03 15:59:29 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,19 @@ variable "sku_name" {
   description = "SKU name for the API Management service"
   default     = "Developer_1"
 }
+variable "client_id" {
+  description = "Azure Service Principal client ID"
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal client secret"
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+}
+

53ea5af41b089c3a30495c959b126a14dde83806
YuanFangXiBei <shilei12369@163.com>
2024-12-03 14:57:25 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "example-resource-group"
-  location = "eastus"
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_api_management" "apim" {
+  name                = var.api_management_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_email     = var.publisher_email
+  publisher_name      = var.publisher_name
 }
